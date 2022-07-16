0ab2b633e4fe7eec7b3e5730cdbabd19993b89ff
Prasad Adhav <prasadadhav22@gmail.com>
2022-07-16 17:23:17 +0530

    Created redux app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css"
+import { useSelector,useDispatch } from "react-redux";
+import { incNumber,decNumber } from './actions/index';
 
-function App() {
+const App = () => {
+  const myState = useSelector((state) => state.changeTheNumber);
+  const dispatch = useDispatch();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container text-center mt-5">
+      <h1>Increment/Decrement Counter</h1>
+      <h4 className="quantity mt-5">using React and Redux</h4>
+      <div className="quantity mt-5">
+        <button title="Decrement" className="quantity_minus btn-secondary btn-sm" onClick={ () => dispatch(decNumber()) }>
+        <span>-</span>
+        </button>
+        <input type="text" name="quantity"
+          className="quantity_input" value={myState}/>
+        
+        <button title="Increment" className="quantity_plus btn-secondary btn-sm" onClick={ () => dispatch(incNumber()) }>
+          <span>+</span>
+        </button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
